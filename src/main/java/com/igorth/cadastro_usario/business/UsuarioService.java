4bb6f072d64462acead0bddf992ddf799d605767
Igor Theophilo <igorth.castro@gmail.com>
2026-01-27 16:43:07 -0300

    CRUD creation

--- src/main/java/com/igorth/cadastro_usario/business/UsuarioService.java
+++ src/main/java/com/igorth/cadastro_usario/business/UsuarioService.java
@@ -2,6 +2,7 @@ package com.igorth.cadastro_usario.business;
 
 import org.springframework.stereotype.Service;
 
+import com.igorth.cadastro_usario.infrastructure.entitys.Usuario;
 import com.igorth.cadastro_usario.infrastructure.repository.UsuarioRepository;
 
 @Service
@@ -12,4 +13,32 @@ public class UsuarioService {
         this.repository = repository;
     }
 
+    public void salvarUsuario (Usuario usuario){
+        repository.saveAndFlush(usuario);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        
+        return repository.findByEmail(email).orElseThrow(
+            () -> new RuntimeException("Email não encontrado")
+        );
+    }
+
+    public void deletarUsuarioPorEmail(String email){
+        repository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorEmail(Integer id, Usuario usuario){
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() -> 
+                new RuntimeException("Usuario não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() :
+                        usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() :
+                        usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+
+        repository.saveAndFlush(usuarioAtualizado);
+    }
 }
