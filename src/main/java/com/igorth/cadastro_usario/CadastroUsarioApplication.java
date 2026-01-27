6b5a3f162cdbb143be58492d7b24eb87a766fd47
Igor Theophilo <igorth.castro@gmail.com>
2026-01-27 15:37:00 -0300

    Project created

---
+++ src/main/java/com/igorth/cadastro_usario/CadastroUsarioApplication.java
@@ -0,0 +1,13 @@
+package com.igorth.cadastro_usario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroUsarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroUsarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorth/cadastro_usario/CadastroUsarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorth.cadastro_usario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
