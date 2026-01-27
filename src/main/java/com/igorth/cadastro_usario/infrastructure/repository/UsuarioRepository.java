b3b612874854ff7e24ee0a4cc70f3ab3f4e4b3bd
Igor Theophilo <igorth.castro@gmail.com>
2026-01-27 16:31:42 -0300

    creating the deleteByEmail method

--- src/main/java/com/igorth/cadastro_usario/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/igorth/cadastro_usario/infrastructure/repository/UsuarioRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.igorth.cadastro_usario.infrastructure.entitys.Usuario;
 
+import jakarta.transaction.Transactional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     
     Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
